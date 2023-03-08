4af24f8e9ca23272391c82d8bc6972a67c96ba36
arafat-rimon <rimonarafat2020@gmail.com>
2023-03-08 23:00:05 +0600

    first commit

---
+++ src/main/java/com/example/hotel_managemet_system/HotelManagemetSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.hotel_managemet_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelManagemetSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HotelManagemetSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/hotel_managemet_system/HotelManagemetSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hotel_managemet_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelManagemetSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
